e5bd05227e9e493a75104f7856b4aabdd2625e80
Ethan Mingerink <etmingerink@yahoo.com>
2023-10-16 08:11:34 -0400

    Add a Print Statement

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -5,3 +5,5 @@
 print("Hello Ethan Mingerink!")
 print("\nI am looking forwared for my birthday next week!")
 print("I am looking fowared to y end of the soccer season party.")
+
+print("It is a monday")
